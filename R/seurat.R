37c12d3e4f40e690ca5b17bc7e601e0282e3f4fe
Daniel Ringwalt <dringwalt@jhu.edu>
2024-03-10 14:08:20 -0400

    In umap, save spca.centered and save the RunUMAP command on the seurat object

--- R/seurat.R
+++ R/seurat.R
@@ -163,9 +163,16 @@ RunSparsePCA <- function(seurat, ...) {
 
 spca_with_centered_umap <- function(seurat, spca, dims=1:50, seed.use=1, umap_transform=diag(2)) {
   seurat[['spca']] = spca
-  seurat[['umap.spca']] = RunUMAP(
-    spca@cell.embeddings[, dims] %>% scale(scale=F, center=T), seed.use=seed.use
+  seurat[['spca.centered']] <- CreateDimReducObject(
+    embeddings = spca@cell.embeddings %>% scale(scale = FALSE, center = TRUE),
+    key = "SPCACEN_",
+    assay = "RNA"
   )
+  seurat <- seurat %>%
+    RunUMAP(
+      reduction = "spca.centered", reduction.name = "umap.spca", dims = dims,
+      seed.use = seed.use
+    )
   dimnames(umap_transform) <- rep(list(colnames(seurat[['umap.spca']])), 2)
   seurat[['umap.spca']]@cell.embeddings <- (
     seurat[['umap.spca']]@cell.embeddings %*% umap_transform
