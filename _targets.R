94c55b4b0e7efdc80f94518e954ce3c35abd7021
Daniel Ringwalt <dringw@gmail.com>
2024-04-07 16:46:14 -0700

    Map the indrop and tenx objects for dot plot data

--- _targets.R
+++ _targets.R
@@ -173,7 +173,7 @@ midgut_figures = list(
     )
   ),
   tar_target(
-    indrop.dot.plot.features,
+    midgut.dot.plot.features,
     c(
       "esg", "Dl", "E(spl)mbeta-HLH", "Rel",
       # dEC-enriched:
@@ -195,8 +195,8 @@ midgut_figures = list(
     save_figure(
       "figure/Midgut/Dot-Plot-Indrop-PCA.pdf",
       midgut_dot_plot(
-        indrop.cpm_pca_subclassif[indrop.dot.plot.features, ],
-        indrop.pct.expressed_pca_subclassif[indrop.dot.plot.features, ],
+        cpm_pca_subclassif_indrop[midgut.dot.plot.features, ],
+        pct.expressed_pca_subclassif_indrop[midgut.dot.plot.features, ],
         midgut.model.colors.bg["PCA"]
       ),
       width = 6.4, height = 4.8, device = cairo_pdf
@@ -207,8 +207,8 @@ midgut_figures = list(
     save_figure(
       "figure/Midgut/Dot-Plot-Indrop-SPCA.pdf",
       midgut_dot_plot(
-        indrop.cpm_spca_subclassif[indrop.dot.plot.features, ],
-        indrop.pct.expressed_spca_subclassif[indrop.dot.plot.features, ],
+        cpm_spca_subclassif_indrop[midgut.dot.plot.features, ],
+        pct.expressed_spca_subclassif_indrop[midgut.dot.plot.features, ],
         midgut.model.colors.bg["SPCA"]
       ),
       width = 6.4, height = 4.8, device = cairo_pdf
@@ -942,32 +942,48 @@ list(
     build_de_data(indrop.glm, indrop.present.genes),
     packages = c("apeglm", "glmGamPoi", "SummarizedExperiment")
   ),
+  # Dot plot data generation for batch crossed with PCA/SPCA workflow.
   tar_map(
-    tibble(name=c("pca_subclassif", "spca_subclassif")),
+    tribble(
+      ~model, ~batch, ~seurat, ~glm_fits,
+      "pca_subclassif", "indrop", rlang::sym("indrop"), rlang::sym("indrop.glm"),
+      "spca_subclassif", "indrop", rlang::sym("indrop"), rlang::sym("indrop.glm"),
+      "pca_subclassif", "tenx", rlang::sym("tenx"), rlang::sym("tenx.glm"),
+      "spca_subclassif", "tenx", rlang::sym("tenx"), rlang::sym("tenx.glm")
+    ) %>%
+      rowwise %>%
+      mutate(
+        glm_model = model %>%
+          str_replace("pca_subclassif", "pca_clusters") %>%
+          rlang::sym() %>%
+          call("$", glm_fits, .) %>%
+          list
+      ),
+    names = c("model", "batch"),
     tar_target(
-      indrop.glm.dispersion.param,
+      dispersion.trend,
       with(
-        indrop.glm[[name %>% str_replace("pca_subclassif", "pca_clusters")]],
+        glm_model,
         overdispersion_shrinkage_list$dispersion_trend %>%
           setNames(names(overdispersions))
       )
     ),
     tar_target(
-      indrop.glm.abundances,
-      build_glm_cpm(indrop, name, indrop.glm.dispersion.param),
+      glm.abundances,
+      build_glm_cpm(seurat, model, dispersion.trend),
       packages = "glmGamPoi"
     ),
     tar_target(
-      indrop.cpm,
-      predict_glm_cpm(indrop.glm.abundances)
+      cpm,
+      predict_glm_cpm(glm.abundances)
     ),
     tar_target(
-      indrop.pct.expressed,
-      indrop[['RNA']] %>%
+      pct.expressed,
+      seurat[['RNA']] %>%
         GetAssayData %>%
         t %>%
         as.data.frame %>%
-        split(FetchData(indrop, name)[, 1]) %>%
+        split(FetchData(seurat, model)[, 1]) %>%
         sapply(\(df) colMeans(as.matrix(df) != 0))
     )
   ),
